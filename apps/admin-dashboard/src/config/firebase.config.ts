9e97a70fd9edcf828f0cea1957a8218f44b84c38
miuracSomnath <miuracdevelopmentsomnath@gmail.com>
2022-04-08 21:20:12 +0530

    add-secret-vars

--- apps/admin-dashboard/src/config/firebase.config.ts
+++ apps/admin-dashboard/src/config/firebase.config.ts
@@ -2,19 +2,10 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
+import { firebaseSecretConfig } from '../secrets';
 
 // Initialize Firebase
-const app = initializeApp({
-  apiKey: 'AIzaSyDVvT190YHjnYh5LRobXI4cQf8F3_vuGOc',
-  authDomain: 'dropoutstore-8979d.firebaseapp.com',
-  databaseURL:
-    'https://dropoutstore-8979d-default-rtdb.asia-southeast1.firebasedatabase.app',
-  projectId: 'dropoutstore-8979d',
-  storageBucket: 'dropoutstore-8979d.appspot.com',
-  messagingSenderId: '300563119027',
-  appId: '1:300563119027:web:fefd76170953e8b14669c6',
-  measurementId: 'G-RXD44LFG2Z',
-});
+const app = initializeApp(firebaseSecretConfig);
 
 const firestore = getFirestore(app);
 const auth = getAuth(app);
