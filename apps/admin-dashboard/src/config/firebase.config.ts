75b1018b1e58215b4b7d7a55e55e4fe7ffa9819d
kirandn <kirandn30@gmail.com>
2022-04-12 12:55:08 +0530

    add pages and routing

--- apps/admin-dashboard/src/config/firebase.config.ts
+++ apps/admin-dashboard/src/config/firebase.config.ts
@@ -2,7 +2,7 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
-import { firebaseSecretConfig } from './secrets';
+import { firebaseSecretConfig } from '../secrets';
 
 // Initialize Firebase
 const app = initializeApp(firebaseSecretConfig);
