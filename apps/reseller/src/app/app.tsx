dc2c78c66b4d60ae4b4d69202d8f33e1ddf64350
kirandn <kirandn30@gmail.com>
2022-04-23 18:36:49 +0530

    Created new reseller app

---
+++ apps/reseller/src/app/app.tsx
@@ -0,0 +1,15 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from './app.module.css';
+import NxWelcome from './nx-welcome';
+
+import { Route, Link } from 'react-router-dom';
+
+export function App() {
+  return (
+    <div>
+
+    </div>
+  );
+}
+
+export default App;
