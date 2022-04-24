4438877ea030c8a3fb656208726f5468e847dc3d
miuractech <miuractech@gmail.com>
2022-04-24 15:10:05 +0530

    initiated ui components with InputField

--- apps/reseller/src/app/app.tsx
+++ apps/reseller/src/app/app.tsx
@@ -1,13 +1,13 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import styles from './app.module.css';
-import NxWelcome from './nx-welcome';
+import './app.css';
 
 import { Route, Link } from 'react-router-dom';
+import InputField from '../UI/input-field/input-field';
 
 export function App() {
   return (
-    <div>
-
+    <div style={{ height:'100vh', backgroundColor:'#FBFAF8'}} className='margin2' >
+      <InputField  />
     </div>
   );
 }
