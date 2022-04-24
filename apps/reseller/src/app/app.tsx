0940b2817f5204af1f95274edfedb5047fe5f98f
miuractech <miuractech@gmail.com>
2022-04-24 15:12:33 +0530

    initiated ui components with InputField

--- apps/reseller/src/app/app.tsx
+++ apps/reseller/src/app/app.tsx
@@ -6,8 +6,7 @@ import InputField from '../UI/input-field/input-field';
 
 export function App() {
   return (
-    <div style={{ height:'100vh', backgroundColor:'#FBFAF8'}} className='margin2' >
-      <InputField  />
+    <div >
     </div>
   );
 }
