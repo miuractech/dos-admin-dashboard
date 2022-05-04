afc7091cd5096b64915752645077b6e25e3aa0eb
miuractech <miuractech@gmail.com>
2022-05-04 11:45:32 +0530

    get OTP timer fix

--- apps/reseller/src/app/Auth/verify-phone/verify-phone.tsx
+++ apps/reseller/src/app/Auth/verify-phone/verify-phone.tsx
@@ -17,7 +17,7 @@ export function VerifyPhone(props: VerifyPhoneProps) {
   const [alert, setAlert] = useState<string>("")
   const [requestOTP, setRequestOTP] = useState(false)
   const [err, setErr] = useState("")
-  const [timer, setTimer] = useState(true)
+  const [timer, setTimer] = useState(false)
   const [seconds, setSeconds] = useState<number>(30)
 
   const currentUser = auth?.currentUser
@@ -38,6 +38,14 @@ export function VerifyPhone(props: VerifyPhoneProps) {
 
     resendOTP({
       onSuccess: () => {
+          const timerId = setInterval(() => {
+            if (seconds === 0) {
+              clearTimeout(timerId);
+              setTimer(true)
+            } else {
+              setSeconds(t => t - 1)
+            }
+          }, 1000);
         setAlert("OTP sent successfully")
         setTimer(true)
       }, onFail: (error: any) => {
@@ -72,14 +80,7 @@ export function VerifyPhone(props: VerifyPhoneProps) {
     }, 3000)
   }
 
-  // const timerId = setInterval(() => {
-  //   if (seconds === -1) {
-  //     clearTimeout(timerId);
-  //     setTimer(false)
-  //   } else {
-  //     setSeconds(t => t - 1)
-  //   }
-  // }, 1000);
+
 
 
 
@@ -107,7 +108,7 @@ export function VerifyPhone(props: VerifyPhoneProps) {
               <h4 style={{ textAlign: "center" }}>Registered Mobile number is - {phone}</h4>
               <Button fullWidth onClick={clicked} type='submit' variant='contained'>GET OTP</Button>
               <p style={{ textAlign: "right" }}>Somthing went wrong? <strong onClick={signOut} style={{ color: '#167AF9', cursor: "pointer" }}>Go Back</strong></p>
-              {timer ? <p><strong>wait for to resend OTP : {seconds} seconds</strong></p> : <p><strong onClick={resend} style={{ color: '#167AF9', cursor: "pointer" }}>Resend OTP</strong></p>}
+              
             </div>
             :
             <div>
@@ -118,7 +119,7 @@ export function VerifyPhone(props: VerifyPhoneProps) {
               <InputField fullWidth color='primary' placeholder="Enter OTP" type="number" style={{ textDecoration: "none" }} forminput={{ ...register("OTP") }} />
               <Button fullWidth type='submit' variant='contained'>verify</Button>
               {err && <Typography variant='caption' color={'error'} >{err}</Typography>}
-
+              {timer ? <p><strong>wait for to resend OTP : {seconds} seconds</strong></p> : <p><strong onClick={resend} style={{ color: '#167AF9', cursor: "pointer" }}>Resend OTP</strong></p>}
             </div>
           }
         </div>
